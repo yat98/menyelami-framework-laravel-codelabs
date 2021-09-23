6623a3c6dee40d7ec8eaba2618b575728c0a710a
yat98 <hidayatchandra08@gmail.com>
2021-09-23 22:31:15 +0800

    menambahkan database - konfigurasi read/write database

--- database/config/database.php
+++ database/config/database.php
@@ -46,7 +46,12 @@ return [
 			'url' => env('DATABASE_URL'),
 			'host' => env('DB_HOST', '127.0.0.1'),
 			'port' => env('DB_PORT', '3306'),
-			'database' => env('DB_DATABASE', 'forge'),
+			'read' => [
+				'database' => env('DB_DATABASE', 'forge'),
+			],
+			'write' => [
+				'database' => env('DB_DATABASE_2', 'forge'),
+			],
 			'username' => env('DB_USERNAME', 'forge'),
 			'password' => env('DB_PASSWORD', ''),
 			'unix_socket' => env('DB_SOCKET', ''),
