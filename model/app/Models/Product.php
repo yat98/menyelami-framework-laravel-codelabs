3c5f007db69261515a69ccceb4930a2e2591bf3f
yat98 <hidayatchandra08@gmail.com>
2021-09-26 18:35:32 +0800

    menambahkan model - softdelete

--- model/app/Models/Product.php
+++ model/app/Models/Product.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
 	use HasFactory;
+	use SoftDeletes;
 
 	public $timestamps = false;
 
@@ -20,4 +22,6 @@ class Product extends Model
 		'name',
 		'price',
 	];
+
+	protected $dates = ['deleted_at'];
 }
