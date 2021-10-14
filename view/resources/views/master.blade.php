5fd7177c23fa30ce5283705a08e288cdb38275f1
yat98 <hidayatchandra08@gmail.com>
2021-10-14 08:40:05 +0800

    menambahkan view - mencoba laravel mix

--- view/resources/views/master.blade.php
+++ view/resources/views/master.blade.php
@@ -6,10 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title','Quote App')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
     @yield('body')
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
