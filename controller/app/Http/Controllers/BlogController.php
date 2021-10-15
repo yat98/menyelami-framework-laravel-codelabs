d8695e663982078fa407d5098bed42d516fc77db
yat98 <hidayatchandra08@gmail.com>
2021-10-15 08:55:59 +0800

    menambahkan controller - penggunaan controller

---
+++ controller/app/Http/Controllers/BlogController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BlogController extends Controller
+{
+	public function getArchive()
+	{
+		return '<h1>Halaman Archive</h1>';
+	}
+}
---
+++ controller/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Api\HomeController;
+use App\Http\Controllers\BlogController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	return view('welcome');
+});
+
+Route::get('about', [HomeController::class, 'about'])
+	->name('home.about');
+
+// Impilicit Controller
+// Route::controller('blogs', BlogController::class);
