198c21f3ca99979e8342bfaf04dad0448fcfc2a8
yat98 <hidayatchandra08@gmail.com>
2021-10-31 15:31:36 +0800

    menambahkan authentikasi authorisasi -reset password

--- authentikasi-authorisasi/app/Models/User.php
+++ authentikasi-authorisasi/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +13,7 @@ class User extends Authenticatable
 	use HasApiTokens;
 	use HasFactory;
 	use Notifiable;
+	use CanResetPassword;
 
 	/**
 	 * The attributes that are mass assignable.
